b501e381a41cb7ed86953bd1bf550a82a967f2bf
jonathan <jonathan.cifuentes2030@gmail.com>
2025-09-16 01:47:54 -0600

    correcion usuario

--- src/app.js
+++ src/app.js
@@ -3,17 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const cookieParser = require("cookie-parser");
-const { PrismaClient } = require("@prisma/client");
 
 const authRoutes = require("./routes/auth.routes");
 const perfilRoutes = require("./routes/perfil.routes");
 const cargaInicialRoutes = require("./routes/cargaInicial.routes");
-const inventarioRoutes = require("./routes/Inventario.routes");
+const inventario = require("./routes/Inventario.routes");
 
 const app = express();
-const prisma = new PrismaClient();
-
-// CORS
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",").map((origin) => origin.trim())
   : [];
@@ -21,60 +17,30 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
 app.use(
   cors({
     origin: function (origin, callback) {
+      // Permite peticiones sin origen (como Postman) o desde los orígenes permitidos
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, origin);
       } else {
         callback(new Error("No permitido por CORS"));
       }
     },
-    credentials: true,
+    credentials: true, // Permite cookies y credenciales
   })
 );
-
 app.use(express.json());
 app.use(cookieParser());
-
-// Proxy de Render
 app.set("trust proxy", 1);
 
-// Rate limit
-const maxPeticiones = process.env.INDEFINIDOPETICIONES
-  ? parseInt(process.env.INDEFINIDOPETICIONES)
-  : 25;
-
+const peticiones = process.env.INDEFINIDOPETICIONES;
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: maxPeticiones,
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: peticiones, // máximo 20 peticiones por IP
   message: "Demasiadas peticiones, intenta más tarde.",
 });
 
 app.use("/api/v1/auth", authLimiter, authRoutes);
 app.use("/api/perfil", perfilRoutes);
 app.use("/api/v1/cargaInicial", cargaInicialRoutes);
-app.use("/api/v1/inventario", inventarioRoutes);
+app.use("/api/v1/inventario", inventario);
 
-// Ruta de prueba
-app.get("/", async (req, res) => {
-  try {
-    const usuarios = await prisma.uSUARIO.findMany({ take: 5 });
-    res.json({ status: "OK", sample: usuarios });
-  } catch (err) {
-    res
-      .status(500)
-      .json({
-        error: "Error al conectarse a la base de datos",
-        details: err.message,
-      });
-  }
-});
-
-// Levantar servidor directamente
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-  console.log(
-    `Render URL: ${
-      process.env.RENDER_EXTERNAL_URL || "http://localhost:" + PORT
-    }`
-  );
-});
+module.exports = app;
